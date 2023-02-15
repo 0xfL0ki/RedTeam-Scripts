0c6a8507a96d6f18c5b2bf6740106d9e6721f624
0xfL0k! <shaifi.moazzem@gmail.com>
2023-02-16 01:20:46 +1100

    Added a new ProcessList C# Project

---
+++ ProcessList/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ProcessList
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateToolhelp32Snapshot(SnapshotFlags dwFlags, uint th32ProcessID);
+        [DllImport("kernel32.dll")]
+        static extern bool Process32First(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct PROCESSENTRY32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ProcessID;
+            public IntPtr th32DefaultHeapID;
+            public uint th32ModuleID;
+            public uint cntThreads;
+            public uint th32ParentProcessID;
+            public int pcPriClassBase;
+            public uint dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szExeFile;
+        };
+
+        [Flags]
+        enum SnapshotFlags : uint
+        {
+            HeapList = 0x00000001,
+            Process = 0x00000002,
+            Thread = 0x00000004,
+            Module = 0x00000008,
+            Module32 = 0x00000010,
+            Inherit = 0x80000000,
+            All = 0x0000001F
+        }
+
+        static void Main(string[] args)
+        {
+            IntPtr snapshot = CreateToolhelp32Snapshot(SnapshotFlags.Process, 0);
+
+            if (snapshot == IntPtr.Zero)
+            {
+                Console.WriteLine("Error creating snapshot");
+                return;
+            }
+
+            PROCESSENTRY32 entry = new PROCESSENTRY32();
+            entry.dwSize = (uint)Marshal.SizeOf(entry);
+
+            if (!Process32First(snapshot, ref entry))
+            {
+                Console.WriteLine("Error retrieving process list");
+                return;
+            }
+
+            Console.WriteLine("PID               Process Name");
+            Console.WriteLine("----              -------------");
+            do
+            {
+                Console.WriteLine("{0}                 {1}", entry.th32ProcessID, entry.szExeFile);
+            } while (Process32Next(snapshot, ref entry));
+        }
+    }
+}
